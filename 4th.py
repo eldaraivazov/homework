783c33dc2a88b8331864e15e6fa412861ac7ea55
Eldar Aivazov <aivazoveldar@gmail.com>
2020-02-04 08:05:40 +0200

    '1'

--- 4th.py
+++ 4th.py
@@ -1,17 +1,27 @@
 from random import randint
-a = int(input('Введіть довжину першого списку'))
-b = int(input('Введіть максимальний елемент першого списку'))
-c = []
-for i in range (a):
-    c.append(randint(0,b ))
-print(c)
-d = int(input('Введіть довжину другого списку'))
-e = int(input('Введіть максимальний елемент другого списку'))
-f = []
-for i in range (d):
-    f.append(randint(0,e ))
-print(f)
-result = list(set(c) & set(f))
-print('І в нас співпали наступні числа: ' + str(result))
-if(result==[]):
-    print('Нажаль співпадіння відсутні')
+a = int(input('Введіть довжину першого списку  '))
+b = int(input('Введіть максимальний елемент першого списку  '))
+c = int(input('Введіть довжину другого списку  '))
+d = int(input('Введіть максимальний елемент другого списку  '))
+def func (e,f,g,h):
+    first = []
+    for i in range(e):
+        first.append(randint(0, f+1))
+
+
+    second = []
+    for i in range(g):
+        second.append(randint(0, h+1))
+
+
+    final = []
+    for g in first:
+        if g in second:
+            final.append(g)
+
+    if len(final) ==0:
+        print('Співпадіння не виявлені')
+        return final
+    else:
+        return list(set(final))
+print (func(a,b,c,d))
